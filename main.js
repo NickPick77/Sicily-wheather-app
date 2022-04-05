78bf859103b44e9b81f27dce784d44ff8a350aa7
Nicolò Piccolo <nicopico77@gmail.com>
2022-04-05 20:07:13 +0200

    PATCH: added select control

--- main.js
+++ main.js
@@ -1,73 +1,293 @@
-const create = (selector) => document.createElement(selector);
-const query = (selector) => document.querySelector(selector);
-const getMeteoData = async (lat, lon) => {
-    const res = await fetch(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2b9d22b5a41dc852a89e0737214e4502`, {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    if (res.status >= 200 && res.status <= 299) {
-    return await res.json();
-  } else {
-    // Redirect
-    // window.location.href = "/";
-
-    const h1El = create("h1");
-    h1El.textContent = "404 pagina non trovata";
-
-    document.body.append(h1El);
-
-    //throw new Error("La pagina non è stata trovata");
-  }
-};
-
-
-const createCard = (main, desc, icon) => {
-    const divEl = create("div");
-            divEl.classList.add("card-container");
-    const h2El = create("h2");
-            h2El.classList.add("main-weath")
-            h2El.textContent = main;
-    const parWeathEl = create("p");
-            parWeathEl.classList.add("Weather");
-            parWeathEl.textContent = desc;   
-    const imgEl = create("img");
-            imgEl.classList.add("icon");
-            imgEl.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`)
-
-    divEl.append(h2El,parWeathEl, imgEl);
-    query(".cards-wrapper").appendChild(divEl);
-}
-//"lon": 13.35976,
-//"lat": 38.115822 palermo
+import { query, queryAll, getMeteoData, createCard } from "./utils.js";
+//https://cors-anywhere.herokuapp.com/
+//CHANGE BACKGROUND DEPEND ON WEATHER
 const changeBackground = () => {
   const val = query(".main-weath").textContent;
-  const Div = query(".card-container")
-  console.log(val);
+  const Div = queryAll(".card-container")
+  //console.log(val);
+  Div.forEach(()=>{
   switch (val) {
     case "Clouds":
-      console.log(Div)
-      Div.classList.add("grey");
-      
+      Div.classList.add("clouds");
       break;
-  
+    case "Clear":
+      Div.classList.add("clear");
+      break;
     default:
       break;
   }
+})
 }
+// ON PAGE LOAD//
+//PALERMO CARD
 getMeteoData(38.115822, 13.35976).then((resultAPI) => {
-  const result = resultAPI.weather
-  console.log(result)
-     
-     result.map((weather) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
       createCard(
           weather.main,
           weather.description,
-          weather.icon
+          weather.icon,
+          cityName
+      )
+      
+  })
+})
+//AGRIGENTO CARD
+getMeteoData(37.320839, 13.58876).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
       )
   })
-}).then(()=> changeBackground());
-; 
-//console.log(getMeteoData(38.115822, 13.35976))
+})
+//CALTANISSETTA CARD
+getMeteoData(37.48774, 14.04497).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+      )
+  })
+})
+//CATANIA CARD
+getMeteoData(37.502129, 15.08719).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+      )
+  })
+})
+//ENNA CARD
+getMeteoData (37.558849, 14.28917).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+      )
+  })
+})
+//MESSINA CARD
+getMeteoData(-22.351311, 30.0396).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+      )
+  })
+})
+//RAGUSA
+getMeteoData(36.928242, 14.71719).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+      )
+  })
+})
+//SIRACUSA CARD
+getMeteoData(37.085152, 15.273).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+      )
+  })
+})
+//TRAPANI CARD
+getMeteoData(38.015839, 12.51077).then((resultAPI) => {
+  const weath = resultAPI.weather
+  const cityName = resultAPI.name
+     weath.map((weather) => {
+      createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+      )
+  })
+})
+const selectEl = query(".cities")
+
+
+
+
+//AT SELECT EVENT//
+selectEl.addEventListener("change", (event) => {
+  const selectedOption = selectEl.value
+  if(selectedOption == ""){
+    const cardContainer = queryAll(".card-container");
+    cardContainer.forEach(element => {
+        element.remove();
+    });
+    //PALERMO CARD
+    getMeteoData(38.115822, 13.35976).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+          
+      })
+    })
+    //AGRIGENTO CARD
+    getMeteoData(37.320839, 13.58876).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    //CALTANISSETTA CARD
+    getMeteoData(37.48774, 14.04497).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    //CATANIA CARD
+    getMeteoData(37.502129, 15.08719).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    //ENNA CARD
+    getMeteoData (37.558849, 14.28917).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    //MESSINA CARD
+    getMeteoData(-22.351311, 30.0396).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    //RAGUSA
+    getMeteoData(36.928242, 14.71719).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    //SIRACUSA CARD
+    getMeteoData(37.085152, 15.273).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    //TRAPANI CARD
+    getMeteoData(38.015839, 12.51077).then((resultAPI) => {
+      const weath = resultAPI.weather
+      const cityName = resultAPI.name
+         weath.map((weather) => {
+          createCard(
+              weather.main,
+              weather.description,
+              weather.icon,
+              cityName
+          )
+      })
+    })
+    }
+  else{
+    //const selectEl = query(".cities")
+    const cardContainer = queryAll(".card-container");
+    cardContainer.forEach(element => {
+      element.remove();
+    });
+    const latLon = selectedOption.split(",");
+    console.log(selectedOption, latLon)
+    getMeteoData(latLon[0], latLon[1]).then((resultAPI) => {
+    const weath = resultAPI.weather
+    const cityName = resultAPI.name
+       weath.map((weather) => {
+        createCard(
+          weather.main,
+          weather.description,
+          weather.icon,
+          cityName
+        )
+    })
+  })
+  }
+})
