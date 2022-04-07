0c7f951f961171997f59f24dbea544cf67b8df68
Nicolò Piccolo <nicopico77@gmail.com>
2022-04-07 13:32:16 +0200

    ADD:  array for option value

--- data.js
+++ data.js
@@ -399,4 +399,48 @@ const Trapani = {
     "name": "Trapani",
     "cod": 200
     };
-export { Palermo, Agrigento, Caltanissetta, Catania, Enna, Messina, Ragusa, Siracusa, Trapani };
+
+//LATITUDE AND LONGITUDE ARRAY
+const latLonValue = [
+    {
+        value: "All",
+        name: "All cities",
+    },
+    {
+    value: "37.320839,13.58876",
+    name : "Agrigento",
+    },
+    {
+    latLon : "37.48774,14.04497",
+    name :  "Caltanissetta",
+    },
+    {
+    latLon : "37.502129,15.08719",
+    name :  "Catania"
+    },
+    {
+    latLon : "37.558849,14.28917",
+    name :  "Enna"
+    },
+    {
+    latLon : "-22.351311,30.0396",
+    name :  "Messina"
+    },
+    {
+    latLon : "38.115822,13.35976",
+    name :  "Palermo"
+    },
+    {
+    latLon : "36.928242,14.71719",
+    name :  "Ragusa"
+    },
+    {
+    latLon : "37.085152,15.273",
+    name :  "Syracuse"
+    },
+    {
+    latLon : "38.015839,12.51077",
+    name : "Trapani"
+    }
+]
+export { Palermo, Agrigento, Caltanissetta, Catania, Enna, Messina, Ragusa, Siracusa, Trapani, latLonValue };
