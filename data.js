5cb97076a3d2169c9f47a88e8ea8245214a4deaa
Nicolò Piccolo <nicopico77@gmail.com>
2022-04-07 13:35:22 +0200

    ADD: array for select option

--- data.js
+++ data.js
@@ -407,40 +407,40 @@ const latLonValue = [
         name: "All cities",
     },
     {
-    value: "37.320839,13.58876",
-    name : "Agrigento",
+        value: "37.320839,13.58876",
+        name : "Agrigento",
     },
     {
-    latLon : "37.48774,14.04497",
-    name :  "Caltanissetta",
+        latLon : "37.48774,14.04497",
+        name :  "Caltanissetta",
     },
     {
-    latLon : "37.502129,15.08719",
-    name :  "Catania"
+        latLon : "37.502129,15.08719",
+        name :  "Catania"
     },
     {
-    latLon : "37.558849,14.28917",
-    name :  "Enna"
+        latLon : "37.558849,14.28917",
+        name :  "Enna"
     },
     {
-    latLon : "-22.351311,30.0396",
-    name :  "Messina"
+        latLon : "-22.351311,30.0396",
+        name :  "Messina"
     },
     {
-    latLon : "38.115822,13.35976",
-    name :  "Palermo"
+        latLon : "38.115822,13.35976",
+        name :  "Palermo"
     },
     {
-    latLon : "36.928242,14.71719",
-    name :  "Ragusa"
+        latLon : "36.928242,14.71719",
+        name :  "Ragusa"
     },
     {
-    latLon : "37.085152,15.273",
-    name :  "Syracuse"
+        latLon : "37.085152,15.273",
+        name :  "Syracuse"
     },
     {
-    latLon : "38.015839,12.51077",
-    name : "Trapani"
+        latLon : "38.015839,12.51077",
+        name : "Trapani"
     }
 ]
 export { Palermo, Agrigento, Caltanissetta, Catania, Enna, Messina, Ragusa, Siracusa, Trapani, latLonValue };
